b731b99fbde89996cf354d755236890f0c3ca08c
HoangHieu <dinhhoanghieu321@gmail.com>
2022-03-02 16:48:08 +0700

    Check result and show next product

---
+++ lib/product_info.dart
@@ -0,0 +1,14 @@
+class ProductInfo {
+  final String name;
+  final int price;
+
+  ProductInfo(this.name, this.price);
+}
+
+final products = [
+  ProductInfo('Wireless mouse', 3),
+  ProductInfo('Keyboard', 5),
+  ProductInfo('Camera', 8),
+  ProductInfo('Speaker', 4),
+  ProductInfo('iPad', 1000),
+];
--- lib/screens/home_page.dart
+++ lib/screens/home_page.dart
@@ -1,8 +1,18 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:name_your_price/product_info.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  int _productIndex = 0;
+  int? _inputtedPrice;
+  String _result = '';
 
   @override
   Widget build(BuildContext context) {
@@ -12,20 +22,52 @@ class HomePage extends StatelessWidget {
         child: Column(
           children: <Widget>[
             const Padding(padding: EdgeInsets.only(top: 80)),
-            const Text('Bluetooth Mouse', style: TextStyle(fontSize: 25)),
+            Text(products[_productIndex].name,
+                style: const TextStyle(fontSize: 25)),
             const Padding(padding: EdgeInsets.only(top: 40)),
             SizedBox(
               width: 200,
               child: TextField(
+                key: const Key('priceInput'),
                 decoration: const InputDecoration(border: OutlineInputBorder()),
                 keyboardType: TextInputType.number,
                 inputFormatters: [FilteringTextInputFormatter.digitsOnly],
+                onChanged: (value) {
+                  _inputtedPrice = int.tryParse(value);
+                },
               ),
             ),
             const Padding(padding: EdgeInsets.only(top: 40)),
             ElevatedButton(
               child: const Text('Check'),
-              onPressed: () {},
+              onPressed: () {
+                setState(() {
+                  _result = _inputtedPrice == products[_productIndex].price
+                      ? 'pass'
+                      : 'fail';
+                });
+              },
+            ),
+            const Padding(padding: EdgeInsets.only(top: 40)),
+            Visibility(
+              visible: _result.isNotEmpty,
+              child: Text(_result,
+                  style: const TextStyle(fontSize: 20),
+                  key: const Key('result')),
+            ),
+            const Padding(padding: EdgeInsets.only(top: 40)),
+            Visibility(
+              visible: _result.isNotEmpty,
+              child: ElevatedButton(
+                  onPressed: () {
+                    setState(() {
+                      _result = '';
+                      if (_productIndex < 4) {
+                        _productIndex++;
+                      }
+                    });
+                  },
+                  child: const Text('Next')),
             )
           ],
         ),
